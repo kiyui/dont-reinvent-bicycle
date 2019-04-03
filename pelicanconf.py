2f92e5444d0076e8ee89e574288b227efb1c7df9
Timur Kiyui <timur@linux.com>
2019-04-04 00:28:43 +0800

    chore(pelican): add initial site configurations

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Timur Kiyui'
+SITENAME = 'dont reinvent велосипед'
+SITEURL = ''
+
+# Sute options
+TIMEZONE = 'Asia/Kuching'
+DEFAULT_LANG = 'en'
+
+# Compilation settings
+PATH = 'content'
+RELATIVE_URLS = True
+DELETE_OUTPUT_DIRECTORY = True
+
+# Disable feed generation while developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Options
+DEFAULT_PAGINATION = 10
+
+# Blogroll
+LINKS = (('GNOME Extensions',
+          'https://extensions.gnome.org/accounts/profile/TimurKiyivinski'),)
+
+# Social widget
+SOCIAL = (('GitHub',
+           'https://github.com/kiyui'),
+          ('LinkedIn',
+           'https://www.linkedin.com/in/timothy-kiyui-a47503180/'),)
